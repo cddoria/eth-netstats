a2e346e4e527b1f703330a8cfdadc509d71b4999
Connor Doria <cddoria@us.ibm.com>
2015-06-03 08:41:49 -0400

    additions to all in one eth-netstats

--- app.js
+++ app.js
@@ -244,3 +244,37 @@ var nodeCleanupTimeout = setInterval( function ()
 server.listen(process.env.PORT || 3000);
 
 module.exports = server;
+
+//api-part
+'use strict';
+
+var nodeModel = require('./lib/node');
+
+var node = new nodeModel();
+
+var gracefulShutdown = function() {
+	console.log('');
+    console.error("xxx", "sys", "Received kill signal, shutting down gracefully.");
+
+    node.stop();
+    console.info("xxx", "sys", "Closed node watcher");
+
+    setTimeout(function(){
+        console.info("xxx", "sys", "Closed out remaining connections.");
+        process.exit(0);
+    }, 5*1000);
+}
+
+// listen for TERM signal .e.g. kill
+process.on('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on('SIGINT', gracefulShutdown);
+
+// listen for shutdown signal from pm2
+process.on('message', function(msg) {
+	if (msg == 'shutdown')
+		gracefulShutdown();
+});
+
+module.exports = node;
